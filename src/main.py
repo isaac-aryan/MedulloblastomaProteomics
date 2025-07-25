673f9fefe6c12df2b405efb5a636b80b624b4aa5
Aryan Isaac Bhobe <bhobe.ai@gmail.com>
2025-07-25 10:53:56 +0100

    initial commit

---
+++ src/main.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_excel('data/raw/proteomics.xlsx')
+md = pd.read_excel('data/raw/metadata.xlsx')
+
+df = df.drop(['entry_name', 'accession_number'], axis=1)
+
+md.columns = md.iloc[0,:]
+
+# Cleaning metadata for this analysis
+md = pd.read_excel('data/raw/metadata.xlsx')
+md.columns = md.iloc[0,:]
+md.drop(index=[0], axis=0)
+
