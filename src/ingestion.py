6479b51e9308bd8b94359e69d373df140b88149c
Aryan Isaac Bhobe <bhobe.ai@gmail.com>
2025-07-27 12:35:59 +0100

    ingestionv1 complete

---
+++ src/ingestion.py
@@ -0,0 +1,55 @@
+import numpy as np
+import pandas as pd
+from collections import defaultdict
+
+def process_expression_data(
+        data:pd.DataFrame,
+        isoforms:bool=False
+        ) -> pd.DataFrame:
+    
+    processed = data.drop(['accession_number'], axis=1)
+
+    duplicates = data['geneSymbol'].value_counts()
+    duplicate_symbols = duplicates[duplicates > 1].index.tolist()
+    print(f'{len(duplicate_symbols)} gene symbols have multiple protein isoforms')
+
+    # Strategy 1 - Keep highest expressed isoform
+    if isoforms is False:
+
+        num_cols = processed.columns[2:]
+        processed['Average'] = processed[num_cols].mean(axis=1)
+        processed = processed.loc[processed.groupby('geneSymbol')['Average'].idxmax()]
+        processed = processed.drop(columns=['Average'])
+        processed = processed.reset_index(drop=True)
+    
+    # Strategy 2 - Keep unique isoforms
+    else:
+
+        counters = defaultdict(int)
+        new_values = []
+        
+        for symbol in processed['geneSymbol']:
+            if symbol in duplicate_symbols:
+                counters[symbol] += 1
+                new_values.append(f"{symbol}_{counters[symbol]}")
+            else:
+                new_values.append(symbol)
+        
+        isoform_series = pd.Series(new_values, index=processed.index)
+        processed['geneSymbol'] = isoform_series
+
+    # Symbol to name mappings:
+    symbol2name = processed[['geneSymbol', 'entry_name']]
+    processed = processed.drop(columns=['entry_name'])
+
+    return processed, symbol2name
+
+def get_expression_matrix(
+        expr_data: pd.DataFrame,
+        index_col: str='geneSymbol'
+) -> pd.DataFrame:
+    
+    expr_mat = expr_data.set_index(index_col)
+
+    return expr_mat
+
--- src/main.py
+++ src/main.py
@@ -1,12 +1,12 @@
 import pandas as pd
 import numpy as np
 
+from ingestion import get_expression_matrix, process_expression_data
+
 df = pd.read_excel('../data/raw/proteomics.xlsx')
 md = pd.read_excel('../data/raw/metadata.xlsx')
 
-df = df.drop(['entry_name', 'accession_number'], axis=1)
-
-df
+# Working on metadata processing
 md.columns = md.iloc[0,:]
 
 # Cleaning metadata for this analysis
@@ -14,3 +14,12 @@ md = pd.read_excel('data/raw/metadata.xlsx')
 md.columns = md.iloc[0,:]
 md.drop(index=[0], axis=0)
 
+
+# Processing expression data
+expression_data, symbol_mappings = process_expression_data(data=df, isoforms=False)
+
+expression_data
+symbol_mappings
+
+matrix = get_expression_matrix(expression_data)
+matrix.head()
